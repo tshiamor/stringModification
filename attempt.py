886bcf4d28931606d55c24c4299024ad8c0d95e4
tshiamor <56265291+tshiamor@users.noreply.github.com>
2023-02-03 11:05:21 +0800

    Add files via upload

--- attempt.py
+++ attempt.py
@@ -7,7 +7,7 @@ import re
 import sys
 import numpy as np
 
-pattern ='[A-Za-z0-9_]'
+pattern =r'[A-Za-z0-9_\s\#\%\!\$]'
 
 def raw_input():
     f = open("test.txt", "r")
@@ -18,73 +18,57 @@ def raw_input():
 first_multiple_input = raw_input().rstrip().split()
 n = int(first_multiple_input[0])
 m = int(first_multiple_input[1])
+matrix = []
+
+for _ in xrange(n):
+    matrix_item = raw_input()
+    matrix.append(matrix_item)
 '''
 
-raw_input2 = re.findall(r'[A-Za-z0-9_\s\#\%\!\$]', raw_input() )
-
-print(raw_input2)
+raw_input2 = re.findall(pattern, raw_input() )
 
 multirow=[]
 i = 0
 width = raw_input2.index('\n')
 while '\n' in raw_input2: raw_input2.remove('\n')
 
+print('----input-----')
 print(raw_input2)
 
+#-------------obtain  array with shape (8,3)---------
 arr = np.asarray(raw_input2)
 newarr = arr.reshape(-1, width)
 
-print(newarr)
-
-print('---iterate ---')
 
 arr = newarr.tolist()
-print(len(arr[0]))
 
 result = []
 for i in range(0, len(arr[0]) ):
   for row in arr:
-    res = re.findall(r'[A-Za-z_\s\#\%\!\$]', row[i])
+    res = re.findall(r'[A-Za-z_\#\%\!\$]', row[i])
     if res:
         result+= res 
     else:
         result += ''
+
+print('----vertical traversal-----')
 print(result)
 result2 = ''.join(result)
 
+#---------- remove initial occurance of #,$,% symbols -------
 result2 = result2.replace('#',' ',1)
 result2 = result2.replace('$','',1)
 result2 = result2.replace('%',' ',1)
 
-#result2 = result2.replace(' ','',1)
+#print(re.sub(r'a-z\sa-z', "", result2))
 
-print(re.sub(r'a-z\sa-z', "", result2))
-
+print('----output-----')
 print(result2)
 
-#result2 = re.sub(r'[\#\$\%)]', '', result2)
-#result2 = re.sub(r'[^#|%|%]', '', result2)
-#result2 = result2.replace('#%',' ',1)
+# target output
+# This is Matrix#  %!
 
 
-#result2 = re.sub(r'[(A-Za-z)\s(A-Za-z)]', '', result2)
-#print(result2)
-
-
-#print(re.sub("/[(A-Za-z)\s(A-Za-z)]/g", '', result2 ))
-
-#print(re.sub("(\#) | (\$) | (\%)", " ", result2))
-
-
-#  This is Matrix#  %!
-
-exit()
-
-matrix = []
-
-for _ in xrange(n):
-    matrix_item = raw_input()
-    matrix.append(matrix_item)
 
 
 
